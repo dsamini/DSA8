aa9a15ee2d07cac6b6db7ae63562e51fb6cca55f
dsamini <2311cs010600@mallareddyuniversity.ac.in>
2025-01-22 11:39:00 +0530

    Number

---
+++ src/com/mru/faqs/ArmStrongNumber.java
@@ -0,0 +1,29 @@
+package com.mru.faqs;
+
+import java.util.Scanner;
+
+public class ArmStrongNumber {
+
+	public static void main(String[] args) {
+		
+		//read number dynamically
+		Scanner scanner = new Scanner(System.in);
+				
+	System.out.print("enter number :");
+     int num = scanner.nextInt();
+     int original_num = num;
+     
+     
+     //read number of digits
+     int digits = String.valueOf(num).length();     //153  ---  3     1634   ---- 4
+     
+     int sum =0 ;
+     while (num!=0) {
+    	 int last = num%10;
+    	 sum+=Math.pow(last,  digits);
+    	 num=num/10;
+     }
+     System.out.println( original_num == sum ? "ArmStrong !!!" : "Not ArmStrong !!!" );
+	}
+
+}
